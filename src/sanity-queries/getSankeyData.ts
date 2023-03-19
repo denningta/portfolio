acf5d616ea6e9094603d305cdc003ad36c4b8bea
Tim Denning <36028402+denningta@users.noreply.github.com>
2023-03-19 15:25:22 +0100

    re-install tailwindcss (styles fixed)

--- src/sanity-queries/getSankeyData.ts
+++ src/sanity-queries/getSankeyData.ts
@@ -1,25 +1,37 @@
 import client from '../lib/sanity-client'
 
-client.fetch(`
-  {
-    "nodes": *[_type in ["employment", "project", "skill"]] {
-      "name": title,
-      "id": slug.current
-    },
-    "linksData": *[_type in ["employment", "project"]] {
-      "links": references[] | {
-        "source": ^.slug.current,
-        "value": percent,
-        ^._type == "employment" => {
-          "target": project->slug.current,
-        },
-        ^._type == "project" => {
-          "target": skill->slug.current
-        },
+const getSankeyData = async () => {
+
+  try {
+    const response = await client.fetch(`
+    {
+      "nodes": *[_type in ["employment", "project", "skill"]] {
+        "name": title,
+        "id": slug.current
+      },
+      "linksData": *[_type in ["employment", "project"]] {
+        "links": references[] | {
+          "source": ^.slug.current,
+          "value": percent,
+          ^._type == "employment" => {
+            "target": project->slug.current,
+          },
+          ^._type == "project" => {
+            "target": skill->slug.current
+          },
+        }
       }
+    } | {
+      "links": linksData[].links[],
+      "nodes": nodes
     }
-  } | {
-    "links": linksData[].links[],
-    "nodes": nodes
+  `)
+
+    return response
+
+  } catch (error: any) {
+    console.log(error)
   }
-`)
+}
+
+export default getSankeyData
