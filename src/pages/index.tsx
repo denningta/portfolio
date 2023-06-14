4373567e36477e7ada23184eef777f30d025a15d
Tim Denning <36028402+denningta@users.noreply.github.com>
2023-06-14 15:08:56 +0200

    changed app title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Tim Denning</title>
         <meta name="description" content="Portfolio Website for Tim Denning" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
